079aa394fc25f6560d3a29bb658d246820276fd0
letammy979 <le.tammy979@gmail.com>
2021-02-04 15:00:18 -0500

    create new tab on click

--- popup.js
+++ popup.js
@@ -1,1 +1,9 @@
-document.querySelector('button')
+document.addEventListener('DOMContentLoaded', function() {
+  const btn = document.querySelector('button');
+  btn.addEventListener('click', function() {
+    const msg = document.createElement('p');
+    msg.innerText = 'hi';
+    document.querySelector('body').appendChild(msg);
+    chrome.tabs.create({url: 'https://www.google.com/'});
+  });
+}, false);
